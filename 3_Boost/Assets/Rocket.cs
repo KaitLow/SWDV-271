7d2eb8fe8ea94922ce6ec8569307d73b83513886
Kait Low <kdlowl@gmail.com>
2020-10-07 15:02:47 -0600

    Added tags for Friendly, Fuel. Added code for collision.

--- 3_Boost/Assets/Rocket.cs
+++ 3_Boost/Assets/Rocket.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
-    [SerializeField] float rcsThrust = 100f;
-    [SerializeField] float mainThrust = 100f;
+    [SerializeField] float rcsThrust = 100.0f;
+    [SerializeField] float mainThrust = 250f;
     Rigidbody rigidBody;
     AudioSource audioSource;
 
@@ -26,23 +26,25 @@ public class Rocket : MonoBehaviour
     private void Rotate()
     {
         rigidBody.freezeRotation = true;
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
         if (Input.GetKey(KeyCode.A))
         {
             
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);           
+            transform.Rotate(-Vector3.forward * rotationThisFrame);           
         }
         rigidBody.freezeRotation = false;
     }
 
     private void Thrust()
     {
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up * rotationThisFrame);
             if (!audioSource.isPlaying)
             {
                 audioSource.Play();
@@ -52,5 +54,25 @@ public class Rocket : MonoBehaviour
         {
             audioSource.Stop();
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                // do nothing, someone I like
+                print("Friendly");
+                break;
+            case "Fuel":
+                //My spaceship is hungry
+                print("Fuel");
+                break;
+            default:
+                // die
+                print("Die!");
+                break;
+        }
+
     }
 }
