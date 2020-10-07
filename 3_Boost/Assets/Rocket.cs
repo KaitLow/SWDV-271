98642c5fd01096142a5f0b22a2cce26cda60d480
Kait Low <kdlowl@gmail.com>
2020-10-07 14:15:30 -0600

    Added thrust audio and obstacles.

--- 3_Boost/Assets/Rocket.cs
+++ 3_Boost/Assets/Rocket.cs
@@ -7,33 +7,50 @@ public class Rocket : MonoBehaviour
     [SerializeField] float rcsThrust = 100f;
     [SerializeField] float mainThrust = 100f;
     Rigidbody rigidBody;
+    AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Rotate();
+        Thrust();
     }
 
-    private void ProcessInput()
+    private void Rotate()
     {
-        if(Input.GetKey(KeyCode.Space))
+        rigidBody.freezeRotation = true;
+        if (Input.GetKey(KeyCode.A))
         {
-            rigidBody.AddRelativeForce(Vector3.up);
-        }
-        
-        if(Input.GetKey(KeyCode.A))
-        {
+            
             transform.Rotate(Vector3.forward);
         }
-        else if(Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward);           
+        }
+        rigidBody.freezeRotation = false;
+    }
+
+    private void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
         }
     }
 }
